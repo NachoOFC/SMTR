3deac5af5b403fa3fa7880a1996bddd170b812c2
Nacho <105531940+NachoOFC@users.noreply.github.com>
2025-05-16 12:30:16 -0400

    cambio UPSooo a UPS

--- pages/index.vue
+++ pages/index.vue
@@ -47,7 +47,7 @@ export default {
       assets: [
         { 
           id: 10, 
-          name: 'Batería UPSoooooo', 
+          name: 'Batería UPS', 
           value: '90%', 
           valueClass: 'badge-green', 
           sector: 'Los Muermos', 

56aa5210c0c14dedaa2fdb8ef20710f2879a52fa
Nacho <105531940+NachoOFC@users.noreply.github.com>
2025-05-15 23:11:04 -0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,14 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
 
+  ssr: false,
+
+
+  // Si usas GitHub Pages, descomenta esta línea y reemplaza "smtr" por el nombre de tu repo:
+  // app: {
+  //   baseURL: '/smtr/'
+  // },
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
